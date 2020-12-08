f2ab9ab245c7f1c889d1dca1e6b9a7895044b234
kipkoech-msojo <collinskipkoech95@gmail.com>
2020-12-08 14:15:00 +0300

    added registration form

--- advice_app/views.py
+++ advice_app/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import Post
+from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
 def home_view(request):
@@ -8,3 +9,10 @@ def home_view(request):
         'posts': posts,
     }
     return render(request,'advice/home.html',context)
+
+def register(request):
+    form = UserCreationForm()
+    context = {
+        'form':form,
+    }
+    return render(request,'advice/register.html',context)
