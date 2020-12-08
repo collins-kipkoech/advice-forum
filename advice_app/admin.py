da47b44c52a1fb4a774f7b4611ced25fe906425a
kipkoech-msojo <collinskipkoech95@gmail.com>
2020-12-08 07:41:13 +0300

    created post model

--- advice_app/admin.py
+++ advice_app/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Post
 
 # Register your models here.
+@admin.register(Post)
+class PostAdmnin(admin.ModelAdmin):
+    list_display = ['content','date_posted','author']
+    list_filter = []
+    search_fields = []
